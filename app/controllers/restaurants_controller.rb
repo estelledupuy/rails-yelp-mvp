84f4a7f1374e5076248a8d55db7dcf7ccfd22b41
estelle dupuy <estelledupuy@yahoo.fr>
2023-02-22 15:22:01 +0100

    with refacto, without css

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new(restaurant_id: @restaurant.id)
   end
 
   def new
